9f052f82cd83ef230152109256e91c201342fbed
mara1104 <marateixeira1104@gmail.com>
2025-02-26 08:27:04 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
+// Intégration de la connexion avec Google
+document.getElementById("login-button").addEventListener("click", function() {
+    gapi.auth2.getAuthInstance().signIn().then(function(googleUser) {
+        let profile = googleUser.getBasicProfile();
+        alert('Bienvenue, ' + profile.getName() + '!');
+    });
+});
 
+function startApp() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init({
+            client_id: 'YOUR_GOOGLE_CLIENT_ID' // Remplace par ton client ID de Google
+        });
+    });
+}
